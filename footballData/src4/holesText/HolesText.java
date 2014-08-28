3a1522f7c471b30fcfdd1551f3814ceed30938a3
Demetrius <dricha714@gmail.com>
2014-08-28 12:43:24 -0400

    Completed HolesTest.text with project description at the top

--- footballData/src4/holesText/HolesText.java
+++ footballData/src4/holesText/HolesText.java
@@ -1,3 +1,17 @@
+/*Chef wrote some text on a piece of paper and now he wants to know how many holes are in the text. 
+ * What is a hole? If you think of the paper as the plane and a letter as a curve on the plane, then 
+ * each letter divides the plane into regions. For example letters "A", "D", "O", "P", "R" divide the 
+ * plane into two regions so we say these letters each have one hole. Similarly, letter "B" has two 
+ * holes and letters such as "C", "E", "F", "K" have no holes. We say that the number of holes in the 
+ * text is equal to the total number of holes in the letters of the text. Help Chef to determine how 
+ * many holes are in the text.
+ * 
+ * Input : The first line contains a single integer T <= 40, the number of test cases. T test cases 
+ * follow. The only line of each test case contains a non-empty text composed only of uppercase letters 
+ * of English alphabet. The length of the text is less then 100. There are no any spaces in the input.
+ * 
+ * Output : For each test case, output a single line containing the number of holes in the corresponding text.*/
+
 package holesText;
 
 import java.io.File;
@@ -14,24 +28,36 @@ public class HolesText {
 			Scanner sc = new Scanner(new File(file));
 			String text;
 			String oneAlpha = "AabDdegOoPpQqR";
-			char B = 'B';
+			int points = 0;		
+			int numTest;
 			
-			text = sc.nextLine();
 			
-			for(int i = 0; i < text.length(); i++){
-				for(int j = 0; j < oneAlpha.length(); j++){
-					if(text.charAt(i) == oneAlpha.charAt(j)){
-						System.out.println(text.charAt(i) + " == " + oneAlpha.charAt(j));
+			//text = sc.next();
+			while(sc.hasNext()){
+				numTest = sc.nextInt();
+				while(numTest != 0){
+					text = sc.nextLine().trim();
+					if(text.length() == 0)
+						continue;
+					for(int i = 0; i < text.length(); i++){
+						for(int j = 0; j < oneAlpha.length(); j++){
+							if(text.charAt(i) == oneAlpha.charAt(j)){
+								//System.out.println(text.charAt(i) + " == " + oneAlpha.charAt(j));
+								points++;
+								break;
+							}
+							if(text.charAt(i) == 'B'){
+								//System.out.println(text.charAt(i) + " == B");
+								points += 2;
+								break;
+							}
+						}
 					}
-				}
-					
-				//System.out.println(text.charAt(i));
+					System.out.println(text + " points: " + points);
+					numTest--;
+					points = 0;
+				}	
 			}
-			
-			
-			/*while(sc.hasNext()){
-				text = sc.nextLine();
-			}*/
 	 }
 
 	 public static void main(String[] args) throws IOException{
